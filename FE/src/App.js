03181e5ff8dba350bcde1580cca1f3561c561585
Bitthal verma <87093213+bcoder4702@users.noreply.github.com>
2022-11-23 22:32:07 +0530

    Update App.js

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,25 +1,3 @@
-
-/*import './App.css';
-import {BrowserRouter as Router , Routes , Route} from "react-router-dom"
-import MainForm from './component/MainForm';
-import ChatRoom from './component/ChatRoom';
-
-
-function App() {
-	return (
-		<div className="container-fluid bg-light text-dark d-flex align-items-center justify-content-center" style={{height: "100vh"}}>
-			<Router>
-				<Routes>
-					<Route index element={<MainForm />} />
-					<Route path="chat/:roomId" element={<ChatRoom />} />
-					<Route path="*" element={<h1>No match</h1>} />
-				</Routes>
-			</Router>
-		</div>
-	);
-}
-
-export default App;*/
 import './App.css';
 import MainForm from './component/MainForm';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
